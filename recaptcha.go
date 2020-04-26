85e5887ee3533c74fcd1ab87a340805da367d2c1
Towan <roma.khasanov.n@gmail.com>
2020-04-26 21:07:34 +0400

    Check public function

--- recaptcha.go
+++ recaptcha.go
@@ -28,11 +28,11 @@ const recaptchaServerName = "https://www.google.com/recaptcha/api/siteverify"
 
 var recaptchaPrivateKey string
 
-// check uses the client ip address, the challenge code from the reCaptcha form,
+// Check uses the client ip address, the challenge code from the reCaptcha form,
 // and the client's response input to that challenge to determine whether or not
 // the client answered the reCaptcha input question correctly.
-// It returns a boolean value indicating whether or not the client answered correctly.
-func check(remoteip, response string) (r RecaptchaResponse, err error) {
+// It returns a RecaptchaResponse object wich contains user response.
+func Check(remoteip, response string) (r RecaptchaResponse, err error) {
 	resp, err := http.PostForm(recaptchaServerName,
 		url.Values{"secret": {recaptchaPrivateKey}, "remoteip": {remoteip}, "response": {response}})
 	if err != nil {
